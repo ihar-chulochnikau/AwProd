f8f0ac38ce53c2964d27439b811e26751b131d3b
ihar_chulochnikau@epam.com <ihar_chulochnikau@epam.com>
2018-11-25 11:21:30 +0300

    updated logging

--- AW-Production/App_Start/LoggerConfig.cs
+++ AW-Production/App_Start/LoggerConfig.cs
@@ -1,4 +1,5 @@
 ﻿using Serilog;
+using System.Configuration;
 
 namespace AW_Production
 {
@@ -6,9 +7,13 @@ namespace AW_Production
     {
         public static void Register()
         {
+            var aureStorageCOnnectionString = ConfigurationManager.AppSettings["serilog:write-to:AzureTableStorage.connectionString"];
+
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .WriteTo.ApplicationInsightsEvents("a4d623cc-5b6f-4568-933f-1d66a9ada932")
+                .WriteTo.AzureTableStorage(aureStorageCOnnectionString, Serilog.Events.LogEventLevel.Information, storageTableName: "serilog")
+                .ReadFrom.AppSettings()
                 .CreateLogger();
         }
     }
